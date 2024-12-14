34f43464fdd5aaa767a2654c9bc723b37139ee8a
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-14 21:11:15 +0400

    Refactored navigation with dynamic menu and smooth scroll functionality

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-scroll"
+import "./Nav.css"
+
+const Nav = ({isMenuOpen}) => {
+  const items = ["home", "about", "skills", "services", "portfolio", "clients", "contact" ]
+  return (
+    <nav className={isMenuOpen ? "nav active" : "nav"}>
+      <div className={isMenuOpen ? "nav-menu active" : "nav-menu"}>
+        {items.map((item, index) => (
+          <Link 
+            to={item}
+            key={index}
+            className="nav-link"
+          >{item}</Link>
+        ))}
+      </div>
+      
+      {/* <ul className="nav-menu">
+        <li className="nav-item"><a href="#home" className="nav-link active">Home</a></li>
+        <li className="nav-item"><a href="#about" className="nav-link">About</a></li>
+        <li className="nav-item"><a href="#skills" className="nav-link">Skills</a></li>
+        <li className="nav-item"><a href="#services" className="nav-link">Services</a></li>
+        <li className="nav-item"><a href="#portfolio" className="nav-link">Portfolio</a></li>
+        <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li>
+      </ul> */}
+    </nav>
+  )
+}
+
+export default Nav
