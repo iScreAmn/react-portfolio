a87ee471bd3683a85b4b7d49350a397235a93991
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-14 21:06:56 +0400

    Implemented interactive header with logo, navigation, and dynamic menu toggle

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,26 @@
-import { mainImg } from "../../assets/images"
+import { useState } from "react";
+import Nav from "../nav/nav"
 import "./Header.css"
+import { TbMenu2 } from "react-icons/tb";
+import { TbMenu3 } from "react-icons/tb";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
-    <div>
-      <h1>Header</h1>
-      <img src={mainImg} alt="Main Image" />
-    </div>
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <a href="#" className="logo">DJ</a>
+          <Nav isMenuOpen={isMenuOpen} />
+          <div className="nav-menu-btn" onClick={toggleMenu}>
+            {isMenuOpen ? <TbMenu3/> : <TbMenu2/>}
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
