86f5684b470b13b0ba4362e012b251d420871171
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-19 00:53:51 +0400

    Add services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,23 @@
+import { services } from "../../data/services"
+import ServiceItem from "./ServiceItem"
+import SectionTitle from "../section-title/SectionTitle"
 import "./Services.css"
+  
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
+    <section className="services section" id="services">
+      <div className="container flex-center">
+      <SectionTitle title="Services" subtitle="Services"/>
+        <div className="services__wrapper">
+          <h3 className="services-description">What I provide</h3>
+          <ul className="services-list">
+            <ServiceItem services={services}/>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Services
