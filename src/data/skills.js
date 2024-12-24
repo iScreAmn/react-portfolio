a4f0be56d14c4cb5cd9460a66d0409835b4d4e08
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-24 16:24:09 +0400

    Added skills data array with proficiency levels for key technologies

---
+++ src/data/skills.js
@@ -0,0 +1,9 @@
+const skills = [
+  { skill: "HTML", level: "95%"},
+  { skill: "CSS", level: "85%"},
+  { skill: "JavaScript", level: "85%"},
+  { skill: "jQuery", level: "80%"},
+  { skill: "PHP", level: "75%"},
+]
+
+export default skills
