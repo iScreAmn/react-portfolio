e4ae2283e670d74b4054ffac8d91b89ae7d36f7b
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-28 21:40:43 +0400

    Add modal data & img data

---
+++ src/data/portfolioData.js
@@ -0,0 +1,48 @@
+import { portfolio1, portfolio2, portfolio3, portfolio4, portfolio5, portfolio6 } from "../assets/images";
+
+const portfolioData = [
+  {
+    id: 1,
+    title: "Ui/Ux design",
+    category: "YouTube",
+    imgSrc: portfolio1,
+    description: "Добро пожаловать в Creative Space – уникальное цифровое пространство, где творчество встречается с технологиями. Наш сайт – это не просто место, а захватывающее путешествие, где каждая прокрутка открывает новые горизонты. Используя передовые библиотеки, такие как ScrollSmoother, ScrollTrigger, и GSAP, мы создали динамичный параллакс-эффект, который вдохновляет и погружает вас в мир креативных идей."
+  },
+  {
+    id: 2,
+    title: "Ui/Ux design",
+    category: "YouTube",
+    imgSrc: portfolio2,
+    description: "Одностраничный, адаптивный сайт, который создан с использованием HTML и CSS. Реализован по правилам flexbox, grid, используется CSS анимация, медиазапросы, подключены шрифты. Файловая структура организована по файловой структуре Nested БЭМ."
+  },
+  {
+    id: 3,
+    title: "Ui/Ux design",
+    category: "YouTube",
+    imgSrc: portfolio3,
+    description: "Discover Georgia is your gateway to exploring the breathtaking landscapes, rich history, and vibrant culture of one of the world's most enchanting destinations. From the majestic peaks of the Caucasus Mountains to the tranquil shores of the Black Sea, Georgia offers adventures for every traveler."
+  },
+  {
+    id: 4,
+    title: "Ui/Ux design",
+    category: "YouTube",
+    imgSrc: portfolio4,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    id: 5,
+    title: "Ui/Ux design",
+    category: "YouTube",
+    imgSrc: portfolio5,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    id: 6,
+    title: "Ui/Ux design",
+    category: "YouTube",
+    imgSrc: portfolio6,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+]
+
+export default portfolioData;
