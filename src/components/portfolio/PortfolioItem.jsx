b006fca3a06b1bbac61e617df9d533b7419ed395
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-28 21:43:46 +0400

    Create modal window for portfolio items with close functionality

---
+++ src/components/portfolio/PortfolioItem.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import PortfolioModal from "./PortfolioModal"
+
+
+const PortfolioItem = ({item}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const openModal = () => {
+    setIsModalOpen(true)
+}
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  return (
+<div className="portfolio-img-card">
+  <div className="img-card" onClick={openModal}>
+      <div className="overlay"></div>
+      <div className="inf">
+          <h3>{item.title}</h3>
+          <span>{item.category}</span>
+      </div>
+      <img src={item.imgSrc} alt={item.title}/> 
+  </div>
+  {isModalOpen && <PortfolioModal item={item} isModalOpen={isModalOpen} closeModal={closeModal} />}
+  
+</div>
+  )
+}
+
+export default PortfolioItem
