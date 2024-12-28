e22eb5316b0267b72c8340b129800161bfe841b1
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-28 22:30:39 +0400

    initialised & imported section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Header, Footer, Home, About, SkillsInfo, Services, Clients, Contacts, Features, Portfolio } from "./components/index"
+import { Header, Footer, Home, About, SkillsInfo, Services, Clients, Contacts, Features, Portfolio, GetInTouch } from "./components/index"
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
           <SkillsInfo/>
           <Services/>
           <Portfolio/>
+          <GetInTouch/>
           <Clients/>
           <Contacts/>
           <Features/>
