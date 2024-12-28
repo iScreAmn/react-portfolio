ce5559538e61fdb975e29791f3065ce4858d3b8f
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-28 21:42:50 +0400

    Add Portfolio section with dynamic rendering of items

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,22 @@
 import "./Portfolio.css"
+import portfolioData from "../../data/portfolioData"
+import SectionTitle from "../section-title/SectionTitle"
+import PortfolioItem from "./PortfolioItem"
 
 const Portfolio = () => {
   return (
-    <div>
-      <h2>Portfolio</h2>
-    </div>
+    <section className="services section" id="portfolio">
+        <div className="container flex-center">
+            <SectionTitle title="Portfolio" subtitle="Portfolio" />
+            <div className="portfolio__wrapper">
+              {portfolioData.map(item => {
+                return (
+                  <PortfolioItem key={item.id} item={item}/>
+                )
+              })}
+            </div>
+        </div>
+    </section>
   )
 }
 
