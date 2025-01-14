c55f8e587c359bb272865d3bb09f70f219e5c0f3
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-14 21:26:14 +0400

    Create Footer component with structured layout and dynamic link groups

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,27 @@
-import "./Footer.css"
+import "./Footer.css";
+import { infoLinks, followLinks } from "../../data/footerData";
+import FooterLinkGroup from "./FooterLinkGroup";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-wrapper container">
+        <div className="about-group">
+          <h2>Dimitri</h2>
+          <p>Frontend Developer</p>
+          <a href="#about">About me</a>
+        </div>
+        <FooterLinkGroup title="More" links={infoLinks} isSocial={false}/>
+        <div className="hr"></div>
+        <FooterLinkGroup title="Follow" links={followLinks} isSocial={true}/>
+        
+      </div>
+      <p className="footer-copyright">
+        © <span className="year">2024</span> by Yandex Practicum, All rights
+        reserved.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
