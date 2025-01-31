c10bb11530d145bf2a4933f27f53bbe40cebcbdf
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 16:22:14 +0400

    Update Portfolio Item with animated image cards and modal view

--- src/components/portfolio/PortfolioItem.jsx
+++ src/components/portfolio/PortfolioItem.jsx
@@ -1,29 +1,42 @@
-import { useState } from "react"
-import PortfolioModal from "./PortfolioModal"
+import { useState } from "react";
+import { motion } from "motion/react";
+import { slideInVariants } from "../../utils/animation";
+import PortfolioModal from "./PortfolioModal";
 
-
-const PortfolioItem = ({item}) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
+const PortfolioItem = ({ item, index }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => {
-    setIsModalOpen(true)
-}
+    setIsModalOpen(true);
+  };
   const closeModal = () => {
-    setIsModalOpen(false)
-  }
+    setIsModalOpen(false);
+  };
   return (
-<div className="portfolio-img-card">
-  <div className="img-card" onClick={openModal}>
-      <div className="overlay"></div>
-      <div className="inf">
+    <motion.div
+      className="portfolio-img-card"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.5 }}
+      custom={index}
+      variants={slideInVariants("top", 0.7, 50, true)}
+    >
+      <div className="img-card" onClick={openModal}>
+        <div className="overlay"></div>
+        <div className="inf">
           <h3>{item.title}</h3>
           <span>{item.category}</span>
+        </div>
+        <img src={item.imgSrc} alt={item.title} />
       </div>
-      <img src={item.imgSrc} alt={item.title}/> 
-  </div>
-  {isModalOpen && <PortfolioModal item={item} isModalOpen={isModalOpen} closeModal={closeModal} />}
-  
-</div>
-  )
-}
+      {isModalOpen && (
+        <PortfolioModal
+          item={item}
+          isModalOpen={isModalOpen}
+          closeModal={closeModal}
+        />
+      )}
+    </motion.div>
+  );
+};
 
-export default PortfolioItem
+export default PortfolioItem;
