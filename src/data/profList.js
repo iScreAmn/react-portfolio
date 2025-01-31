c1afcb3e03951a183595be82ade1148e64f1a3b0
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 13:35:13 +0400

    Add professional achievements list

---
+++ src/data/profList.js
@@ -0,0 +1,17 @@
+export const profList = [
+  {
+    id: 1,
+    number: "2+",
+    text: "Years of experience",
+  },
+  {
+    id: 2,
+    number: "20+",
+    text: "Happy Customers",
+  },
+  {
+    id: 3,
+    number: "5+",
+    text: "Success Projects",
+  },
+];
