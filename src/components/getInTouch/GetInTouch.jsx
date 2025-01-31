ddbb5639530d5d124a3637d2bbac78f2228310f7
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 16:24:34 +0400

    Animate Contact Card with slide-in effects for title and link

--- src/components/getInTouch/GetInTouch.jsx
+++ src/components/getInTouch/GetInTouch.jsx
@@ -1,26 +1,40 @@
-import "./GetInTouch.css"
+import { motion } from "motion/react";
+import "./GetInTouch.css";
 import { FaPaperPlane } from "react-icons/fa";
+import { slideInVariants } from "../../utils/animation";
 
 const GetInTouch = () => {
   return (
     <div className="get-in-touch sub-section">
       <div className="container flex-center">
-        <div className="contact-card">
+        <motion.div
+          className="contact-card"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          custom={2}
+          variants={slideInVariants("left", 0.7, 50, true)}
+        >
           <div className="title">
             <h4>Let`s talk</h4>
             <h3>About your</h3>
             <h2>Next project</h2>
           </div>
-          <a
+          <motion.a
             href="https://t.me/iscreamn"
             target="_blank"
             rel="noopener noreferrer"
             className="get-in-touch-link inner-info-link"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            custom={2}
+            variants={slideInVariants("right", 0.7, 50, true)}
           >
             Contact me
             <FaPaperPlane />
-          </a>
-        </div>
+          </motion.a>
+        </motion.div>
       </div>
     </div>
   );
