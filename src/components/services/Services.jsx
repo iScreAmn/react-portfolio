e42d2625bf9faeb28be1a4c56ff2a0795fb6c0f3
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 16:18:58 +0400

    Add Services section with animated description and service items

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,23 +1,33 @@
-import services from "../../data/services"
-import ServiceItem from "./ServiceItem"
-import SectionTitle from "../section-title/SectionTitle"
-import "./Services.css"
-  
+import { motion } from "motion/react";
+import { slideInVariants } from "../../utils/animation";
+import services from "../../data/services";
+import ServiceItem from "./ServiceItem";
+import SectionTitle from "../section-title/SectionTitle";
+import "./Services.css";
 
 const Services = () => {
   return (
     <section className="services section" id="services">
       <div className="container flex-center">
-      <SectionTitle title="Services" subtitle="Services"/>
+        <SectionTitle title="Services" subtitle="Services" />
         <div className="services__wrapper">
-          <h3 className="services-description">What I provide</h3>
+          <motion.h3
+            className="services-description"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            custom={1}
+            variants={slideInVariants("top", 0.7, 50, true)}
+          >
+            What I provide
+          </motion.h3>
           <ul className="services-list">
-            <ServiceItem services={services}/>
+            <ServiceItem services={services} />
           </ul>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default Services
+export default Services;
