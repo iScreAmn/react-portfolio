b02db27d83a4e43e70b66b26026a2545d1e087a3
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 16:21:10 +0400

    Update Portfolio section with dynamic item rendering & motion rendering

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,23 +1,21 @@
-import "./Portfolio.css"
-import portfolioData from "../../data/portfolioData"
-import SectionTitle from "../section-title/SectionTitle"
-import PortfolioItem from "./PortfolioItem"
+import "./Portfolio.css";
+import portfolioData from "../../data/portfolioData";
+import SectionTitle from "../section-title/SectionTitle";
+import PortfolioItem from "./PortfolioItem";
 
 const Portfolio = () => {
   return (
     <section className="services section" id="portfolio">
-        <div className="container flex-center">
-            <SectionTitle title="Portfolio" subtitle="Portfolio" />
-            <div className="portfolio__wrapper">
-              {portfolioData.map(item => {
-                return (
-                  <PortfolioItem key={item.id} item={item}/>
-                )
-              })}
-            </div>
+      <div className="container flex-center">
+        <SectionTitle title="Portfolio" subtitle="Portfolio" />
+        <div className="portfolio__wrapper">
+          {portfolioData.map((item, index) => {
+            return <PortfolioItem key={item.id} item={item} index={index} />;
+          })}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
