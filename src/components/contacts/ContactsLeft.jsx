06dfa03781507544ad417675e0d4fbc90bef8e5f
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 16:26:29 +0400

    Add animations to left contact information block and list items

--- src/components/contacts/ContactsLeft.jsx
+++ src/components/contacts/ContactsLeft.jsx
@@ -1,19 +1,35 @@
 import contactsData from "../../data/contactsData";
+import { motion } from "motion/react";
+import { slideInVariants } from "../../utils/animation";
 
 const ContactsLeft = () => {
   return (
     <div className="contact-left">
-      <h2>Let`s discuss your project</h2>
+      <motion.h2
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={slideInVariants("top", 0.7, 50, false)}
+      >
+        Let`s discuss your project
+      </motion.h2>
       <ul className="contact-list">
-        {contactsData.map((item) => (
-          <li key={item.id}>
+        {contactsData.map((item, index) => (
+          <motion.li
+            key={item.id}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            custom={index}
+            variants={slideInVariants("top", 0.7, 50, true)}
+          >
             <h3>
               <item.icon /> {item.title}
             </h3>
             <span>
               <a href={item.link}>{item.value}</a>
             </span>
-          </li>
+          </motion.li>
         ))}
       </ul>
     </div>
