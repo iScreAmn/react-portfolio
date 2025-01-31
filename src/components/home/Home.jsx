ad9526838103d95d7d94329cd42b884fc1258f21
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 13:37:07 +0400

    Implement animated blocks in home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,45 +1,105 @@
-import "./Home.css"
-import { mainImg } from "../../assets/images"
+import "./Home.css";
+import { mainImg } from "../../assets/images";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
 import { FaArrowDownLong } from "react-icons/fa6";
+import { motion } from "motion/react";
+import { iconVariants, slideInVariants } from "../../utils/animation";
 
+const icons = [
+  {
+    id: 1,
+    href: "https://www.facebook.com/jmukhadze.dimitri/",
+    icon: <FaFacebookF />,
+  },
+  {
+    id: 2,
+    href: "https://www.instagram.com/d.jmukhadze/",
+    icon: <FaInstagram />,
+  },
+  { id: 3, href: "https://github.com/iScreAmn/", icon: <FaGithub /> },
+];
 
 const Home = () => {
   return (
     <section className="home" id="home">
-        <div className="container home__wrapper">
-            <div className="media-icons">
-                <a href="https://www.facebook.com/jmukhadze.dimitri/" target="_blank">
-                    <FaFacebookF/>
-                </a>
-                <a href="https://www.instagram.com/d.jmukhadze/" target="_blank">
-                    <FaInstagram/>
-                </a>
-                <a href="https://github.com/iScreAmn/" target="_blank">
-                    <FaGithub/>
-                </a>
-            </div>
-            <div className="home-info">
-                <h1>Hey, I am D.J</h1>
-                <h3>Front-end Developer</h3>
-                <p>I create stunning websites for your business, Highly experienced in web design and development.</p>
-                <a href="#" className="home-info-link inner-info-link">
-                    Contact me
-                    <FaRegArrowAltCircleRight />
-                </a>
-            </div>
-            <div className="home-img">
-                <img src={mainImg} alt="Profile Photo"/>
-            </div>
+      <div className="container home__wrapper">
+        <div className="media-icons">
+          {icons.map((item, index) => (
+            <motion.a
+              href={item.href}
+              key={item.id}
+              target="_blank"
+              custom={index}
+              variants={iconVariants}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.5 }}
+            >
+              {item.icon}
+            </motion.a>
+          ))}
         </div>
-        <a href="#about" className="scroll-down">Scroll-Down
-          <FaArrowDownLong className="arrow-icon"/>
-        </a>
+        <div className="home-info">
+          <motion.h1
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            custom={0}
+            variants={slideInVariants("left", 0.5, 100, true)}
+          >
+            Hey, I am D.J
+          </motion.h1>
+          <motion.h3
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            custom={1}
+            variants={slideInVariants("left", 0.7, 100, true)}
+          >
+            Front-end Developer
+          </motion.h3>
+          <motion.p
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            custom={2}
+            variants={slideInVariants("left", 0.9, 100, true)}
+          >
+            I create stunning websites for your business, Highly experienced in
+            web design and development.
+          </motion.p>
+          <motion.a
+            href="#"
+            className="home-info-link inner-info-link"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            custom={2}
+            variants={slideInVariants("left", 0.9, 100, true)}
+          >
+            Contact me
+            <FaRegArrowAltCircleRight />
+          </motion.a>
+        </div>
+        <motion.div
+          className="home-img"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={slideInVariants("right", 0.9, 100, true)}
+        >
+          <img src={mainImg} alt="Profile Photo" />
+        </motion.div>
+      </div>
+      <a href="#about" className="scroll-down">
+        Scroll-Down
+        <FaArrowDownLong className="arrow-icon" />
+      </a>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
