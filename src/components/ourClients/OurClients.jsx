620b101494f0dedd2963adf920fcb835a31b1e9c
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 16:23:39 +0400

    Enhance OurClients section with animated Swiper integration & motion effect

--- src/components/ourClients/OurClients.jsx
+++ src/components/ourClients/OurClients.jsx
@@ -6,13 +6,21 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import SectionTitle from "../section-title/SectionTitle";
+import { motion } from "motion/react";
+import { slideInVariants } from "../../utils/animation";
 
 const OurClients = () => {
   return (
     <section className="section our-client" id="clients">
       <div className="container flex-center">
         <SectionTitle title="Our clients" subtitle="Our clients" />
-        <div className="our-client-wrapper">
+        <motion.div
+          className="our-client-wrapper"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={slideInVariants("bottom", 0.7, 70, false)}
+        >
           <Swiper
             modules={[Navigation, Pagination]}
             spaceBetween={30}
@@ -29,9 +37,7 @@ const OurClients = () => {
                     <img src={client.imgSrc} alt={client.name} />
                   </div>
                   <div className="client-details">
-                    <p>
-                      {client.description}
-                    </p>
+                    <p>{client.description}</p>
                     <h3>{client.name}</h3>
                     <span>{client.position}</span>
                   </div>
@@ -39,7 +45,7 @@ const OurClients = () => {
               </SwiperSlide>
             ))}
           </Swiper>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
