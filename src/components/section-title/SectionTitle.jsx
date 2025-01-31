83428292431eaaa9af202898d70cf88d02ce4614
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 13:43:47 +0400

    Add animated title and subtitle to SectionTitle component

--- src/components/section-title/SectionTitle.jsx
+++ src/components/section-title/SectionTitle.jsx
@@ -1,12 +1,30 @@
-import "./SectionTitle.css"
+import { motion } from "motion/react";
+import { titleVariants, subtitleVariants } from "../../utils/animation";
+import "./SectionTitle.css";
 
-const SectionTitle = ({title, subtitle}) => {
+const SectionTitle = ({ title, subtitle }) => {
   return (
     <>
-      <h2 className="inner-title">{title}</h2>
-      <h3 className="inner-subtitle">{subtitle}</h3>
+      <motion.h2
+        className="inner-title"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={titleVariants}
+      >
+        {title}
+      </motion.h2>
+      <motion.h3
+        className="inner-subtitle"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={subtitleVariants}
+      >
+        {subtitle}
+      </motion.h3>
     </>
-  )
-}
+  );
+};
 
-export default SectionTitle
+export default SectionTitle;
