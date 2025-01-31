1dfd5b8d7efc042c01ef499966d36e86e1c2d830
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-01-31 13:42:44 +0400

    Enhance About page with animated elements and professional highlights

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,47 +1,91 @@
-import { aboutImg } from "../../assets/images"
-import SectionTitle from "../section-title/SectionTitle"
-import "./About.css"
+import { motion } from "motion/react";
+import { aboutImg } from "../../assets/images";
+import SectionTitle from "../section-title/SectionTitle";
+import { slideInVariants } from "../../utils/animation";
+import { profList } from "../../data/profList";
+import "./About.css";
 
 const About = () => {
   return (
     <section className="about section" id="about">
-        <div className="container flex-center">
-            <SectionTitle title="About me" subtitle="About me"/>
-            <div className="about__wrapper">
-                <div className="about-img">
-                    <img src={aboutImg} alt="about"/>
-                </div>
-                <div className="about-info">
-                    <div className="description">
-                        <h3>I`m Dimitri Jmukhadze</h3>
-                        <h4><span>Front-End Developer</span> based in <span>Georgia</span></h4>
-                        <p>I design and develop services for customers specializing creating stylish, 
-                            modern websites, web services and online stores. 
-                            My passion is to design digital user experiences through meaningful interactions. 
-                            Check out my Portfolio</p>
-                    </div>
-                    <ul className="professional-list">
-                        <li className="list-item">
-                            <span className="number">5+</span>
-                            <span className="text">Years of experience</span>
-                        </li>
-                        <li className="list-item">
-                            <span className="number">3K+</span>
-                            <span className="text">Happy Costumers</span>
-                        </li>
-                        <li className="list-item">
-                            <span className="number">5+</span>
-                            <span className="text">Success Projects</span>
-                        </li>
-                    </ul>
-                    <a href="" className="inner-info-link">Download
-                        <i className="fa-solid fa-download"></i>
-                    </a>
-                </div>
+      <div className="container flex-center">
+        <SectionTitle title="About me" subtitle="About me" />
+        <div className="about__wrapper">
+          <motion.div
+            className="about-img"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={slideInVariants("left", 0.9, 100, false)}
+          >
+            <img src={aboutImg} alt="about" />
+          </motion.div>
+          <div className="about-info">
+            <div className="description">
+              <motion.h3
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                custom={0}
+                variants={slideInVariants("right", 0.5, 50, true)}
+              >
+                I`m Dimitri Jmukhadze
+              </motion.h3>
+              <motion.h4
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                custom={0}
+                variants={slideInVariants("left", 0.5, 50, true)}
+              >
+                <span>Front-End Developer</span> based in <span>Georgia</span>
+              </motion.h4>
+              <motion.p
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                custom={0}
+                variants={slideInVariants("left", 0.5, 50, true)}
+              >
+                I design and develop services for customers specializing
+                creating stylish, modern websites, web services and online
+                stores. My passion is to design digital user experiences through
+                meaningful interactions. Check out my Portfolio
+              </motion.p>
             </div>
+            <ul className="professional-list">
+              {profList.map((item, index) => (
+                <motion.li
+                  className="list-item"
+                  key={item.id}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  custom={index}
+                  variants={slideInVariants("right", 0.6, 40, true)}
+                >
+                  <span className="number">{item.number}</span>
+                  <span className="text">{item.text}</span>
+                </motion.li>
+              ))}
+            </ul>
+            <motion.a
+              href=""
+              className="inner-info-link"
+              custom={3}
+              variants={slideInVariants("bottom", 0.7, 60, true)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+            >
+              Download
+              <i className="fa-solid fa-download"></i>
+            </motion.a>
+          </div>
         </div>
-     </section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
