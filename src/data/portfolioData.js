48923ab36a2d8d1c4887c9eccc137b874bf97ab5
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-01 23:06:07 +0400

    Updated images datas

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,25 +1,25 @@
-import { portfolio1, portfolio2, portfolio3, portfolio4, portfolio5, portfolio6 } from "../assets/images";
+import { modalFirst, modalSecond, modalThird, portfolio4, portfolio5, portfolio6 } from "../assets/images";
 
 const portfolioData = [
   {
     id: 1,
     title: "Ui/Ux design",
     category: "YouTube",
-    imgSrc: portfolio1,
+    imgSrc: modalFirst,
     description: "Добро пожаловать в Creative Space – уникальное цифровое пространство, где творчество встречается с технологиями. Наш сайт – это не просто место, а захватывающее путешествие, где каждая прокрутка открывает новые горизонты. Используя передовые библиотеки, такие как ScrollSmoother, ScrollTrigger, и GSAP, мы создали динамичный параллакс-эффект, который вдохновляет и погружает вас в мир креативных идей."
   },
   {
     id: 2,
     title: "Ui/Ux design",
     category: "YouTube",
-    imgSrc: portfolio2,
+    imgSrc: modalSecond,
     description: "Одностраничный, адаптивный сайт, который создан с использованием HTML и CSS. Реализован по правилам flexbox, grid, используется CSS анимация, медиазапросы, подключены шрифты. Файловая структура организована по файловой структуре Nested БЭМ."
   },
   {
     id: 3,
     title: "Ui/Ux design",
     category: "YouTube",
-    imgSrc: portfolio3,
+    imgSrc: modalThird,
     description: "Discover Georgia is your gateway to exploring the breathtaking landscapes, rich history, and vibrant culture of one of the world's most enchanting destinations. From the majestic peaks of the Caucasus Mountains to the tranquil shores of the Black Sea, Georgia offers adventures for every traveler."
   },
   {
