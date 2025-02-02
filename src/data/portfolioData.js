f4f62e8789a6116a7c90f6358600d0bb8257ce08
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-02 16:27:11 +0400

    Integrated dynamic links into modal data for enhanced interactivity

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -6,6 +6,7 @@ const portfolioData = [
     title: "Creative Space",
     category: "Paralax",
     imgSrc: modalFirst,
+    href: "https://iscreamn.github.io/creative-paralax/",
     description: "Добро пожаловать в Creative Space – уникальное цифровое пространство, где творчество встречается с технологиями"
   },
   {
@@ -13,6 +14,7 @@ const portfolioData = [
     title: "Burger House",
     category: "",
     imgSrc: modalSecond,
+    href: "https://iscreamn.github.io/burger-house-site/",
     description: "Новый ресторан с современными бургерами которы снесут вам крышу"
   },
   {
@@ -20,6 +22,7 @@ const portfolioData = [
     title: "GeoTrip",
     category: "",
     imgSrc: modalThird,
+    href: "https://iscreamn.github.io/site-geo-trip/",
     description: "Discover Georgia is your gateway to exploring the breathtaking landscapes, rich history, and vibrant culture of one of the world's most enchanting destinations. From the majestic peaks of the Caucasus Mountains to the tranquil shores of the Black Sea, Georgia offers adventures for every traveler."
   },
   {
@@ -27,6 +30,7 @@ const portfolioData = [
     title: "Russian Travel",
     category: "Yandex Practicum",
     imgSrc: modalFourth,
+    href: "https://iscreamn.github.io/russian-travel/",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
   {
@@ -34,6 +38,7 @@ const portfolioData = [
     title: "Project Mesto",
     category: "Yandex Practicum",
     imgSrc: modalFifth,
+    href: "#!",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
   {
@@ -41,6 +46,7 @@ const portfolioData = [
     title: "Web Larek",
     category: "Yandex Practicum",
     imgSrc: modalSixth,
+    href: "#",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
 ]
