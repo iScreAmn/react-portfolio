14a4e1de6586c64a195251dc6352da699f21ab80
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-02 14:44:19 +0400

    Updated information in data

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,46 +1,46 @@
-import { modalFirst, modalSecond, modalThird, portfolio4, portfolio5, portfolio6 } from "../assets/images";
+import { modalFirst, modalSecond, modalThird, modalFourth, modalFifth, modalSixth } from "../assets/images";
 
 const portfolioData = [
   {
     id: 1,
-    title: "Ui/Ux design",
-    category: "YouTube",
+    title: "Creative Space",
+    category: "Paralax",
     imgSrc: modalFirst,
-    description: "Добро пожаловать в Creative Space – уникальное цифровое пространство, где творчество встречается с технологиями. Наш сайт – это не просто место, а захватывающее путешествие, где каждая прокрутка открывает новые горизонты. Используя передовые библиотеки, такие как ScrollSmoother, ScrollTrigger, и GSAP, мы создали динамичный параллакс-эффект, который вдохновляет и погружает вас в мир креативных идей."
+    description: "Добро пожаловать в Creative Space – уникальное цифровое пространство, где творчество встречается с технологиями"
   },
   {
     id: 2,
-    title: "Ui/Ux design",
-    category: "YouTube",
+    title: "Burger House",
+    category: "",
     imgSrc: modalSecond,
-    description: "Одностраничный, адаптивный сайт, который создан с использованием HTML и CSS. Реализован по правилам flexbox, grid, используется CSS анимация, медиазапросы, подключены шрифты. Файловая структура организована по файловой структуре Nested БЭМ."
+    description: "Новый ресторан с современными бургерами которы снесут вам крышу"
   },
   {
     id: 3,
-    title: "Ui/Ux design",
-    category: "YouTube",
+    title: "GeoTrip",
+    category: "",
     imgSrc: modalThird,
     description: "Discover Georgia is your gateway to exploring the breathtaking landscapes, rich history, and vibrant culture of one of the world's most enchanting destinations. From the majestic peaks of the Caucasus Mountains to the tranquil shores of the Black Sea, Georgia offers adventures for every traveler."
   },
   {
     id: 4,
-    title: "Ui/Ux design",
-    category: "YouTube",
-    imgSrc: portfolio4,
+    title: "Russian Travel",
+    category: "Yandex Practicum",
+    imgSrc: modalFourth,
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
   {
     id: 5,
-    title: "Ui/Ux design",
-    category: "YouTube",
-    imgSrc: portfolio5,
+    title: "Project Mesto",
+    category: "Yandex Practicum",
+    imgSrc: modalFifth,
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
   {
     id: 6,
-    title: "Ui/Ux design",
-    category: "YouTube",
-    imgSrc: portfolio6,
+    title: "Web Larek",
+    category: "Yandex Practicum",
+    imgSrc: modalSixth,
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
 ]
