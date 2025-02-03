7ea70e4a2c1bc32b4d4726d13970950c0880cd6e
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-03 18:54:09 +0400

    Enabled closing modals with ESC key for faster user interaction

--- src/components/services/ServiceModal.jsx
+++ src/components/services/ServiceModal.jsx
@@ -5,29 +5,24 @@ import { FaXmark } from "react-icons/fa6";
 const ServiceModal = ({ item, isModalOpen, closeModal }) => {
   
   const modalRef = useRef(null);
-
-  // Функция для закрытия модального окна при клике вне его области
   const handleClickOutside = (event) => {
     if (modalRef.current && !modalRef.current.contains(event.target)) {
-      closeModal(); // Закрываем модальное окно
+      closeModal();
     }
   };
 
-  // Добавляем обработчик события при монтировании компонента
   useEffect(() => {
     if (isModalOpen) {
       document.addEventListener("mousedown", handleClickOutside);
     }
 
-    // Удаляем обработчик события при размонтировании компонента
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [isModalOpen]); // Зависимость от isModalOpen
+  }, [isModalOpen]);
 
 
   
-  // Обработчик для закрытия модального окна по нажатию на Escape
   useEffect(() => {
     const handleEscape = (event) => {
       if (event.key === "Escape") {
@@ -35,18 +30,15 @@ const ServiceModal = ({ item, isModalOpen, closeModal }) => {
       }
     };
 
-    // Добавляем обработчик, если модальное окно открыто
     if (isModalOpen) {
       document.addEventListener("keydown", handleEscape);
     }
 
-    // Удаляем обработчик при размонтировании компонента или закрытии модального окна
     return () => {
       document.removeEventListener("keydown", handleEscape);
     };
-  }, [isModalOpen]); // Зависимость от isActive
+  }, [isModalOpen]);
 
-  // Если модальное окно не открыто, ничего не рендерим
   if (!isModalOpen) return null;
 
   return (
