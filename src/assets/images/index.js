16b26d2338219a4a55da2fdd4fa1c38b59bd763b
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-12-24 21:32:30 +0400

    Updated images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,4 @@
 import aboutImg from "./about-img-2.webp"
-import flameJumper from "./flameJumper.jpg"
 
 // Portfolio Images
 import portfolio1 from "./portfolio/askchef.webp"
@@ -11,6 +10,35 @@ import portfolio6 from "./portfolio/telegram.webp"
 import portfolio7 from "./portfolio/ukraine.webp"
 import portfolio8 from "./portfolio/burgerhouse.webp"
 import portfolio9 from "./portfolio/geotrip.webp"
+
+// Diagnoz Clinic
+import portfolio10 from "./portfolio/diagnoz/diagnoz-cover.png"
+import portfolio11 from "./portfolio/diagnoz/1.png"
+import portfolio12 from "./portfolio/diagnoz/2.png"
+import portfolio13 from "./portfolio/diagnoz/3.png"
+import portfolio14 from "./portfolio/diagnoz/4.png"
+
+// Impera Design
+import portfolio15 from "./portfolio/impera/impera-cover.png"
+import portfolio16 from "./portfolio/impera/1.png"
+import portfolio17 from "./portfolio/impera/2.png"
+import portfolio18 from "./portfolio/impera/3.png"
+import portfolio19 from "./portfolio/impera/4.png"
+
+// GPS
+import portfolio20 from "./portfolio/gps/gps-cover.png"
+import portfolio21 from "./portfolio/gps/1.png"
+import portfolio22 from "./portfolio/gps/2.png"
+import portfolio23 from "./portfolio/gps/3.png"
+import portfolio24 from "./portfolio/gps/4.png"
+
+// Flame Jumper
+import flameJumper from "./flameJumper.jpg"
+import flameJumper1 from "./portfolio/flame-jumper/flame-jumper.png"
+import flameJumper2 from "./portfolio/flame-jumper/game-presentation.gif"
+
+
+// Clients
 import client1 from "./client1.webp"
 import client2 from "./client2.webp"
 import client3 from "./client3.webp"
@@ -22,6 +50,10 @@ import skillLogo3 from "./skills/js.webp"
 import skillLogo4 from "./skills/react.webp"
 import skillLogo5 from "./skills/ts.webp"
 
+// Hobby
+import hobby1 from "./hobby/drone1.png"
+import hobby2 from "./hobby/droneFly.png"
+import hobby3 from "./hobby/dronePult.png"
 
 export {
   aboutImg,
@@ -35,6 +67,23 @@ export {
   portfolio7,
   portfolio8,
   portfolio9,
+  portfolio10,
+  portfolio11,
+  portfolio12,
+  portfolio13,
+  portfolio14,
+  portfolio15,
+  portfolio16,
+  portfolio17,
+  portfolio18,
+  portfolio19,
+  portfolio20,
+  portfolio21,
+  portfolio22,
+  portfolio23,
+  portfolio24,
+  flameJumper1,
+  flameJumper2,
   client1,
   client2,
   client3,
@@ -43,4 +92,7 @@ export {
   skillLogo3,
   skillLogo4,
   skillLogo5,
+  hobby1,
+  hobby2,
+  hobby3,
 }
