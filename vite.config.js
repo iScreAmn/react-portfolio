cdd333c0aec07f0b08e68ad650c0cb21d67be2c8
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-01-11 16:19:03 +0400

    Updated vite.config for chunk limits

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            if (id.includes("lenis")) return "lenis";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
