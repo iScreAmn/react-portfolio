83b0beda309f944a844a1077de4d31d6ca5b8ce8
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-01-13 15:02:46 +0400

    Fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 800,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("lenis")) return "lenis";
-            return "vendor";
-          }
-        },
+        manualChunks: undefined, // Отключаем manual chunks для избежания проблем с порядком загрузки
       },
     },
   },
