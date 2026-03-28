bfd9599101a2dfa723d063defbf4dee9a6d6854c
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-03-28 12:56:04 +0400

    Implemented language switching locales-context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { LocaleProvider } from "./context/LocaleContext.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <LocaleProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </LocaleProvider>
 )
